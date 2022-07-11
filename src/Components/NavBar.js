a6d5dc7becb32cfcd2404c1460c012e01a17357b
Christopher Marzulli <chriszulli15@gmail.com>
2022-07-11 11:32:54 -0400

    commit tree 0a410ac2a97fb0bb9a8ed95f4d039c9420af6f2c

---
+++ src/Components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+function NavBar() {
+   
+}
+
+export default NavBar;
